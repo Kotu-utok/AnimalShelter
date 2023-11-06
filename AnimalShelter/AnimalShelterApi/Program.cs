113c2ab3d8bd1efacc9fcd118c97bc2311a0026d
Fatma Zohra Ryma Hadid <fatma-zohra-ryma.hadid.1@ulaval.ca>
2023-11-05 21:59:49 -0500

    sdsd

--- AnimalShelter/AnimalShelterApi/Program.cs
+++ AnimalShelter/AnimalShelterApi/Program.cs
@@ -14,22 +14,9 @@ RegisterCDAServices.RegisterTypeServiceCollectionExtention(builder.Services);
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    c.RoutePrefix = string.Empty; // To serve the Swagger UI at the app's root (https://<your-app-name>.azurewebsites.net/), set RoutePrefix to an empty string.
-});
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllers();
 
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGet("/", context => {
-        context.Response.Redirect("/swagger");
-        return Task.CompletedTask;
-    });
-    endpoints.MapControllers(); // Maps routes to controllers
-});
 app.Run();
