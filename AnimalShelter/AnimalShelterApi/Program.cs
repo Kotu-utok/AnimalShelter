53eb7772cfbccb1068b2445c6c019c7ca1aa1a9d
Fatma Zohra Ryma Hadid <fatma-zohra-ryma.hadid.1@ulaval.ca>
2023-11-05 19:12:05 -0500

    sads

--- AnimalShelter/AnimalShelterApi/Program.cs
+++ AnimalShelter/AnimalShelterApi/Program.cs
@@ -3,19 +3,45 @@ using DAL;
 using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+string connectionString = String.Empty;
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddAutoMapper(typeof(Program));
+
+RegisterCDAServices.RegisterTypeServiceCollectionExtention(builder.Services);
+
+//if(builder.Environment.EnvironmentName == "Local")
+//{
+//    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//}
+//else
+//{
+//    string keyVaultUri = builder.Configuration["KEY_VAULT_URI"];
+//    if(!string.IsNullOrEmpty(keyVaultUri))
+//    {
+//        builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+//    }
+//    else
+//    {
+//        throw new InvalidOperationException("Key Vault URI must be set in the environment variables.");
+//    }
+//    connectionString = builder.Configuration["ConnectionString"];
+//}
+
+//RegisterDALServices.ManageDepenciesDAL(builder.Services, connectionString);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
-
-app.UseRouting(); // This should be before UseAuthorization and endpoint mapping
-
-app.UseAuthorization();
-
+app.UseRouting();
 app.MapControllers();
+
 
 app.UseEndpoints(endpoints =>
 {
@@ -25,6 +51,4 @@ app.UseEndpoints(endpoints =>
     });
     endpoints.MapControllers(); // Maps routes to controllers
 });
-
 app.Run();
-
